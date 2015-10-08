43d4baaa0d9a7e4caf84087d23aaa619af4426bd
Ruthia He <rujiahe@gmail.com>
2015-10-07 17:49:45 -0700

    Gif walkthrough

--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  tips
 //
-//  Created by Rujia He on 10/7/15.
+//  Created by Ruthia He on 10/7/15.
 //  Copyright (c) 2015 SUNLIGHT. All rights reserved.
 //
 
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        var tipPercentages = [0.18, 0.2, 0.22]
+        var tipPercentage = tipPercentages [tipControl.selectedSegmentIndex]
+        
+        var billAmounts = billField.text
+        var billAmount : Double = NSString(string: billAmounts).doubleValue
+        
+//        var billAmount = billField.text.bridgeToObjectiveC().doubleValue
+        var tip = billAmount * tipPercentage
+        var total = billAmount + tip
+        
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 }
 
